1ae6d5f88707b114d9fc616aefa1d5029c73d7d8
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-12-21 15:39:51 +0100

    First Exercise made

--- index.php
+++ index.php
@@ -1,3 +1,51 @@
+<?php
+class Movie
+{
+
+    public $name;
+    public $genre;
+    public $desc;
+    public $rating;
+    public $director;
+
+    function __construct(string $name, string $genre, string $desc, float $rating, string $director)
+    {
+        $this->name = $name;
+        $this->genre = $genre;
+        $this->desc = $desc;
+        $this->rating = $rating;
+        $this->director = $director;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    public function getDesc()
+    {
+        return $this->desc;
+    }
+
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    public function getDirector()
+    {
+        return $this->director;
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,9 +57,14 @@
 </head>
 
 <body>
-    <?php
-    require_once('movie.php');
-    ?>
+
+    <h1>
+        <?php
+        $matrix = new Movie("Matrix", "SciFI", "A movie about a savior in a dystopic world", 8.9, "Wachowski Brothers");
+        $rings = new Movie("The Lord of the Rings", "Fantasy", "A movei about a savior in a fantasy world", 8.8, "Peter Jackson");
+        echo var_dump($matrix, $rings)
+        ?>
+    </h1>
 
 </body>
 
